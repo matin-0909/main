1f554eeefaefb9c2f178c6699d5e32b3e4159f77
matin-0909 <matinj0909@gmail.com>
2026-02-14 16:17:22 +0530

    your commit message

---
+++ src/components/Success.js
@@ -0,0 +1,29 @@
+import React from "react";
+import kisses from "../kisses.gif";
+
+/**
+ * Success component displaying a sweet message upon acceptance.
+ *
+ * @returns {JSX.Element} JSX element representing the Success component.
+ */
+const Success = () => (
+  <div className="App-success">
+    {/* Displaying a cute kisses gif */}
+    <img className="App-gif" src={kisses} alt="Kisses" />
+    {/* Personalized success messages */}
+    <p className="App-text-success">
+     You're my favorite movie buddy, my princess, and the one I enjoy every moment with.
+With you, even the simplest things feel special — whether we're watching a film, talking about nothing, or just sitting quietly together.
+You make my days brighter, my laughs louder, and my heart calmer.
+Being with you feels like home, like happiness I didn’t know I needed.
+I’m so grateful for every smile, every memory, and every second we share. 💖✨
+    </p>
+    <p className="App-text-success">
+      Looking forward to spending Valentine's Day with you, Sakshi.
+    </p>
+    {/* Displaying the date of the special moment */}
+    <p className="App-text-date">02/05/2023</p>
+  </div>
+);
+
+export default Success;
